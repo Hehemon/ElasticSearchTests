8bcca68da0ad55acaaf412423313cf19a6ee75e4
Hehemon <hehemon@gmail.com>
2017-11-08 00:17:24 +0300

    Added several tests for ElasticSearch low client

--- ElasticSearchLearningTests/Tests/HelloWorldTests.cs
+++ ElasticSearchLearningTests/Tests/HelloWorldTests.cs
@@ -8,6 +8,9 @@ namespace ElasticSearchLearningTests.Tests
 {
     public class HelloWorldTests : BaseDependencyResolutionTests
     {
+        private const string DefaultIndex = "people";
+        private const string DefaultType = "person";
+
         private readonly ConnectionConfiguration _defaultConfiguration;
         private readonly ITestOutputHelper _output;
 
@@ -20,12 +23,42 @@ namespace ElasticSearchLearningTests.Tests
         [Fact]
         public void HelloWorld()
         {
+            const string id = "1";
             var lowlevelClient = new ElasticLowLevelClient(_defaultConfiguration);
             var person = new Person {FirstName = "Hello", LastName = "World"};
-            var indexResponse = lowlevelClient.Index<byte[]>("people", "person", "1", person);
+            var indexResponse = lowlevelClient.Index<byte[]>(DefaultIndex, DefaultType, id, person);
             _output.WriteLine(indexResponse.DebugInformation);
-            var responseBody = indexResponse.Body;
-            Assert.NotNull(responseBody);
+            Assert.True(indexResponse.Success);
+        }
+
+        [Fact]
+        public void WriteAndDeleteData()
+        {
+            const string id = "2";
+            var lowlevelClient = new ElasticLowLevelClient(_defaultConfiguration);
+            var person = new Person {FirstName = "John", LastName = "Doe"};
+            var indexResponse = lowlevelClient.Index<string>(DefaultIndex, DefaultType, id, person);
+            _output.WriteLine($"WriteRespose: {indexResponse.DebugInformation}");
+            Assert.True(indexResponse.Success);
+
+            var deleteResponse = lowlevelClient.Delete<string>(DefaultIndex, DefaultType, id);
+            _output.WriteLine($"DeleteResponse: {deleteResponse.DebugInformation}");
+            Assert.True(deleteResponse.Success);
+        }
+
+        [Fact]
+        public async void WriteAsyncAndDeleteData()
+        {
+            const string id = "3";
+            var lowlevelClient = new ElasticLowLevelClient(_defaultConfiguration);
+            var person = new Person { FirstName = "John", LastName = "Doe" };
+            var indexResponse = await lowlevelClient.IndexAsync<string>(DefaultIndex, DefaultType, id, person);
+            _output.WriteLine($"WriteRespose: {indexResponse.DebugInformation}");
+            Assert.True(indexResponse.Success);
+
+            var deleteResponse = lowlevelClient.Delete<string>(DefaultIndex, DefaultType, id);
+            _output.WriteLine($"DeleteResponse: {deleteResponse.DebugInformation}");
+            Assert.True(deleteResponse.Success);
         }
     }
 }
